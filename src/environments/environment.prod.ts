c65e71e66d1d0c9a6fbcec7d588cded6aa2224df
Steve Kent <steve_kent@beaerospace.com>
2018-08-15 11:47:39 +0100

    Updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   accountServer: "https://techdevs-accounts.azurewebsites.net",
-  identityServer: "https://techdevs-identityserver.azurewebsites.net",
-  clientAddress: "https://techdevs-dpmobile.netlify.com",
+  clientAddress: "https://techdevs-custportal.netlify.com",
   identityClientId: "spa",
 };
